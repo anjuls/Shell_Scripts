068dede38304b83789017fcff44d89e9e30c724a
Anjul Sahu <anjulsahu@gmail.com>
2014-09-16 15:06:08 +1000

    Initial version

---
+++ dirsize.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/sh
+# DIRSIZE.sh 
+# Purpose: to list out the directory content sorted by size which is also human readable
+# Author: anjulsahu@gmail.com 
+
+du -sk ./* | sort -n | awk 'BEGIN{ pref[1]="K"; pref[2]="M"; pref[3]="G";} { total = total + $1; x = $1; y = 1; while( x > 1024 ) { x = (x + 1023)/1024; y++; } printf("%g%s\t%s\n",int(x*10)/10,pref[y],$2); } END { y = 1; while( total > 1024 ) { total = (total + 1023)/1024; y++; } printf("Total: %g%s\n",int(total*10)/10,pref[y]); }'
